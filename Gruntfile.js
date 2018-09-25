74576372db7348c0e1fb28f22043a7cf868f4a4b
akrah <krahenbuhl@unistra.fr>
2018-09-26 01:12:45 +0200

    - Ajout des SVG dans le ZIP - Mise à jour du thème IUTRS - Mise à jour du plugin Menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function(grunt) {
 					"lib/{<%= conf.libs %>}",
 					"plugin/<%= conf.plugins %>/**",
 					"plugin/{<%= conf.plugins %>}/**",
-					root[1]+'/img/**.{jpg,jpeg,png,gif}',
+					root[1]+'/img/**.{jpg,jpeg,png,gif,svg}',
 					'index.html',
 				],
 				dest: root[1]+'/reveal-js-presentation.zip'
